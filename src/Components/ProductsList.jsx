7adaff960fabdba4898530e0eb8480c60349a6fb
ChristianScarabelli <scachmabol@hotmail.it>
2025-03-06 10:56:45 +0100

    ADD: milestone 2

--- src/Components/ProductsList.jsx
+++ src/Components/ProductsList.jsx
@@ -1,4 +1,9 @@
+import { useState } from "react"
+
 export default function ProductsList() {
+
+    // Stato per i prodotti aggiunti al carrello
+    const [addedProducts, setAddedProducts] = useState([])
 
     const products = [
         { name: 'Mela', price: 0.5 },
@@ -7,18 +12,47 @@ export default function ProductsList() {
         { name: 'Pasta', price: 0.7 },
     ]
 
+    // Funzione per aggiungere un prodotto al carrello
+    // accetta il prodotto mappato, restituisce il prodotto aggiunto e l'array con tutti i prodotti aggiunti
+    // e aggiungo la propiretà quantity al prodotto
+    const addToCart = (productIndex) => {
+        const product = products[productIndex] // Prendo il prodotto usando l'indice
+        if (addedProducts.some(p => p.index === productIndex)) return    // se il prodotto è già presente nel carrello interrompo
+        const newProduct = { ...product, quantity: 1, index: productIndex }
+        setAddedProducts([newProduct, ...addedProducts])
+    }
+
     return (
         <section>
-            <h2>Lista dei prodotti</h2>
-            <ul>
-                {products.map((p, i) => {
-                    return <li key={i}>
-                        <span>{p.name}</span>
-                        <span>{`${p.price}€`}</span>
-                    </li>
-                })
-                }
-            </ul>
+            <div>
+                <h2>Lista dei prodotti</h2>
+                <ul>
+                    {products.map((p, i) => {
+                        return <li key={i}>
+                            <span>{p.name}</span>
+                            <span>{`${p.price}€`}</span>
+                            <button onClick={() => addToCart(i)}>Add to cart</button>
+                        </li>
+                    })
+                    }
+                </ul>
+            </div>
+            <div>
+                {addedProducts.length > 0 ? (
+                    <div style={{ display: 'flex', flexDirection: 'column', gap: '5px' }}>
+                        <h3>Prodotti nel Carrello</h3>
+                        {addedProducts.map((p) => (
+                            <div key={p.index} style={{ display: 'flex', gap: '10px' }}>
+                                <span>{p.name}</span>
+                                <span>{`${p.price}€`}</span>
+                                <span>Quantità: {p.quantity}</span>
+                            </div>
+                        ))}
+                    </div>
+                ) : (
+                    <h4>Il carrello è vuoto</h4>
+                )}
+            </div>
         </section>
     )
 }
